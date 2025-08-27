9e1944937aa84c44cd03c58fac509aadae0236e9
YongERong <yong_e-rong@s2017.sst.edu.sg>
2025-08-27 19:20:30 +0800

    feat: tiktok scraper

---
+++ README.md
@@ -0,0 +1,13 @@
+How to use:
+1. Create tiktok account, use Cookie Editor extension to extract ms-token cookie
+2. Create a copy of example.env and name it .env, place your ms-token inside
+3. Create a python venv and install the project requirements:
+```python
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+4. Run scrape.py
+```
+python scrape.py
+```
---
+++ scrape.py
@@ -0,0 +1,34 @@
+import asyncio
+import os
+import json
+import logging
+
+import aiofiles
+from dotenv import load_dotenv
+from TikTokApi import TikTokApi
+
+load_dotenv()
+
+logging.basicConfig(level=logging.DEBUG)
+ms_token = os.getenv("TIKTOK_MSTOKEN")
+n = 30  # Batch size, adjust as required
+
+
+async def trending_videos():
+    async with aiofiles.open("data.jsonl", "a+") as f:
+        async with TikTokApi(logging_level=logging.DEBUG) as api:
+            await api.create_sessions(
+                ms_tokens=[ms_token],
+                num_sessions=1,
+                sleep_after=3,
+                browser=os.getenv("TIKTOK_BROWSER", "chromium"),
+                timeout=10800000  # 3 hours, can set longer as required
+            )
+            async for video in api.trending.videos(count=n):
+                logging.info("Adding Video...")
+                await f.write(json.dumps(video.as_dict) + "\n")
+
+
+if __name__ == "__main__":
+    while True:
+        asyncio.run(trending_videos())
