cba9a289382ab802fbda273c31a223fc48b59c50
Julian <juliandominic@yahoo.com.sg>
2025-08-28 21:56:07 +0800

    feat: added SHAP Bar Chart

---
+++ frontend/src/components/SHAPDashboard.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { BarChart } from '@mui/x-charts/BarChart';
+import { Paper } from '@mui/material';
+
+const SHAPDashboard = ({ data, dataKey }) => {
+  return (
+    <Paper elevation={2} sx={{ width: "80%", p: 3, borderRadius: "1.5rem" }}>
+      <Stack width="100%">
+        <Typography variant="h6" component="span" textAlign="center">
+          SHAP Dashboard
+        </Typography>
+        <BarChart
+          dataset={data}
+          height={400}
+          layout="horizontal"
+          margin={{ right: 0, left: 0 }}
+          series={[{ dataKey: "importance", label: "SHAP Value", type: 'bar', stack: 'stack', },]}
+          yAxis={[
+            {
+              dataKey: dataKey,
+              width: 60,
+              disableLine: true,
+              disableTicks: true,
+            },
+          ]}
+          xAxis={[
+            {
+              disableLine: true,
+              disableTicks: true,
+              domainLimit() {
+                return { min: -1.04, max: 1.04 }
+              },
+              colorMap: {
+                type: 'piecewise',
+                thresholds: [0, 1],
+                colors: ['red', 'blue'],
+              }
+            },
+          ]}
+        grid={{ vertical: true }}
+        />
+        {/* <Typography variant="caption">Source: KOSIS</Typography> */}
+      </Stack>
+    </Paper>
+  );
+}
+
+export default SHAPDashboard;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,14 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
 import type { HomeProps } from "../types";
+import { Stack } from "@mui/material";
+import SHAPDashboard from "../components/ShapDashboard";
+
+const mockData = [
+  { feature: "feat1", importance: 0.5 },
+  { feature: "feat2", importance: -0.1 },
+  { feature: "feat3", importance: -0.4 }
+];
 
 const Home = ({ user }: HomeProps) => {
   const navigate = useNavigate();
@@ -10,7 +18,12 @@ const Home = ({ user }: HomeProps) => {
     }
   }, [user, navigate])
   return (
-    user ? <>User</> : <p>No user</p>
+    <Stack sx={{
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <SHAPDashboard data={mockData} dataKey={"feature"} />
+    </Stack>
   );
 };
 
