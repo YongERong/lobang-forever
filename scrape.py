d5a8265af3585a6998dabff6016ac04f697d0e06
YongERong <yong_e-rong@s2017.sst.edu.sg>
2025-08-29 13:23:29 +0800

    feat: add comment scraping functionality

--- scrape.py
+++ scrape.py
@@ -15,18 +15,24 @@ n = 30  # Batch size, adjust as required
 
 
 async def trending_videos():
-    async with aiofiles.open("data.jsonl", "a+") as f:
+    async with aiofiles.open("data4.jsonl", "a+") as f:
         async with TikTokApi(logging_level=logging.DEBUG) as api:
             await api.create_sessions(
                 ms_tokens=[ms_token],
                 num_sessions=1,
                 sleep_after=3,
                 browser=os.getenv("TIKTOK_BROWSER", "chromium"),
-                timeout=10800000  # 3 hours, can set longer as required
+                timeout=10800000,  # 3 hours, can set longer as required
+                headless=False,
             )
             async for video in api.trending.videos(count=n):
+                comments = []
+                async for comment in video.comments(count=30):  # adjust as required
+                    comments.append(comment.as_dict)
+                video_data = video.as_dict
+                video_data["comments"] = comments
                 logging.info("Adding Video...")
-                await f.write(json.dumps(video.as_dict) + "\n")
+                await f.write(json.dumps(video_data) + "\n")
 
 
 if __name__ == "__main__":
