b30e791fcd288a5c9d2a1087ae278e081e203ef2
shauntjw18 <shaun.teojiwu@gmail.com>
2025-08-30 18:33:36 +0800

    Update BERT Training Model.py

--- BERT Training Model.py
+++ BERT Training Model.py
@@ -1,8 +1,9 @@
 from datasets import load_dataset
+from huggingface_hub import HfApi
+import os
 dataset = load_dataset('imdb')
-print(dataset)
-small_train_dataset = dataset["train"].shuffle(seed=42).select([i for i in list(range(3000))])
-small_test_dataset = dataset["test"].shuffle(seed=42).select([i for i in list(range(300))])
+small_train_dataset = dataset["train"].shuffle(seed=42).select([i for i in list(range(5000))])
+small_test_dataset = dataset["test"].shuffle(seed=42).select([i for i in list(range(500))])
 
 
 from transformers import AutoTokenizer
@@ -33,19 +34,21 @@ def compute_metrics(eval_pred):
    f1 = load_f1.compute(predictions=predictions, references=labels)["f1"]
    return {"accuracy": accuracy, "f1": f1}
 
-from huggingface_hub import notebook_login
-notebook_login("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")
+from huggingface_hub import HfApi
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+
 
 
 from transformers import TrainingArguments, Trainer
-repo_name = "lOBANG-FOREVER"
+repo_name = "bert"
 
 training_args = TrainingArguments(
    output_dir=repo_name,
    learning_rate=2e-5,
    per_device_train_batch_size=16,
    per_device_eval_batch_size=16,
-   num_train_epochs=2,
+   num_train_epochs=4,
    weight_decay=0.01,
    save_strategy="epoch",
    push_to_hub=True,
@@ -65,12 +68,4 @@ trainer.train()
 trainer.evaluate()
 trainer.push_to_hub(commit_message="Training complete")
 
-from datasets import load_dataset
-
-model1 = load_dataset("Lyreck/tiktok_brat_comments")
-
-from transformers import pipeline
- 
-sentiment_model = pipeline(model1)
-sentiment_model(["this fucking ate", "she's julia", "i love you", "i hate you" ])
 
