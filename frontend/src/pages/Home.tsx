bad4bebcc3dad76802a19d5497a687066eefb0a1
YongERong <yong_e-rong@s2017.sst.edu.sg>
2025-08-30 10:11:04 +0800

    chore: test connecting frontend and backend

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import type { HomeProps } from "../types";
 import { Divider, Stack } from "@mui/material";
@@ -9,6 +10,14 @@ import MetricCard from "../components/MetricCard";
 import VariableInputForm from "../components/ModelInput";
 
 const Home = ({ user, setUser }: HomeProps) => {
+  const [message, setMessage] = useState<string>("");
+
+  useEffect(() => {
+        axios.get("/api/score?video_duration_sec=30&verified_status=1&author_ban_status=0&like_ratio=0.1&share_ratio=0.05&comment_ratio=0.02")
+            .then(response => setMessage(response.data.score))
+            .catch(error => console.error("Error fetching data", error));
+    }, []);
+
   const navigate = useNavigate();
   useEffect(() => {
     if (!user) {
@@ -27,6 +36,8 @@ const Home = ({ user, setUser }: HomeProps) => {
       spacing={2}
       divider={<Divider flexItem />}
     >
+      // TODO: Temp test to see if it is calling API
+      <h1>{message}</h1>
       <Stack direction={{xs: "column", md: "row"}} spacing={2}>
         {metricData.map((data) => <MetricCard data={data}/>)}
       </Stack>
